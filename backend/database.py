af1f0fec0ecdac8b76abd6e33e14be09d6c818fe
EnricoVavassori1 <enrico.vavassori@supsi.ch>
2026-02-25 18:19:34 +0100

    Backend setup for first tasks

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,14 @@
+import os
+
+
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://simpleuser:password@localhost/db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
@@ -28,18 +28,86 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/items", response_model=schemas.Item)
-def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    db_item = models.Item(name=item.name)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
-
-@app.get("/items", response_model=list[schemas.Item])
-def read_items(db: Session = Depends(get_db)):
-    return db.query(models.Item).all()
 
 @app.get("/")
 def root():
     return {"message": "go to ...../docs for the SWAGGER"}
+
+
+
+
+@app.post("/projects", response_model=schemas.Project)
+def create_project(project: schemas.ProjectCreate, db : Session = Depends(get_db)):
+    db_project = models.Project(name = project.name)
+    db.add(db_project)
+    db.commit()
+    db.refresh(db_project)
+    return db_project
+
+
+
+
+@app.get("/projects", response_model=list[schemas.Project])
+def read_projects(db: Session = Depends(get_db)):
+    return db.query(models.Project).all()
+
+
+
+
+@app.get("/projects/{project_id}", response_model=schemas.Project)
+def get_project_by_id(project_id: int, db:Session = Depends(get_db)):
+    project = db.query(models.Project).filter(models.Project.id == project_id).first()
+    if project is None:
+        raise HTTPException(status_code=404, detail="project not found")
+    return project
+
+
+@app.delete("/projects/{project_id}")
+def delete_project(project_id: int, db: Session = Depends(get_db)):
+    project = db.query(models.Project).filter(models.Project.id == project_id).first()
+    if project is None:
+        raise HTTPException(status_code=404, detail="project not found")
+    db.delete(project)
+    db.commit()
+    return {"message": "Project deleted"}
+
+
+
+@app.post("/projects/{project_id}/images", response_model=schemas.Image)
+def add_image(project_id: int, image: schemas.ImageCreate, db : Session = Depends(get_db)):
+    
+    project = db.query(models.Project).filter(models.Project.id == project_id).first()
+    if project is None:
+        raise HTTPException(status_code=404, detail="Project not found")
+    
+    db_image = models.Image(
+        project_id = project_id,
+        fileName = image.fileName,
+        filePath = image.filePath
+    )
+    
+    db.add(db_image)
+    db.commit()
+    db.refresh(db_image)
+    return db_image
+
+
+@app.get("/projects/{project_id}/images", response_model=list[schemas.Image])
+def read_project_images(project_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Image).filter(models.Image.project_id == project_id).all()
+
+
+
+@app.delete("/images/{image_id}")
+def delete_image(image_id: int, db: Session = Depends(get_db)):
+    image = db.query(models.Image).filter(models.Image.id == image_id).first()
+    if image is None:
+        raise HTTPException(status_code=404, detail="image not found")
+    db.delete(image)
+    db.commit()
+    return {"message": "Image deleted"}
+
+
+
+
+
