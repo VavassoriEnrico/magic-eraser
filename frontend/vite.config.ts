ffd59ffe1f45da289539870931d8ebe46319e0d1
luca <abateluc@cvut.cz>
2026-04-12 12:49:41 +0200

    client tests working and passing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineVitestConfig({
   plugins: [react()],
   server: {
     port: 5173,
   },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  }
 });
+
+
